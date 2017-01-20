02aceeab05f98d7e7642fdd2bb575aef388d0c07
yihengz <chuyathang@outlook.com>
2017-01-19 19:22:44 -0500

    constantlib

---
+++ Mathematica/Assets/Scripts/NewBehaviourScript1.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class NewBehaviourScript1 : MonoBehaviour {
+    public int test = 0;
+}
+
+[CustomEditor(typeof(NewBehaviourScript1))]
+public class NewEditor : Editor {
+    public override void OnInspectorGUI () {
+        DrawDefaultInspector();
+
+        NewBehaviourScript1 tmp = (NewBehaviourScript1)target;
+        if (GUILayout.Button("try")) {
+            tmp.test++;
+        }
+    }
+}
---
+++ Mathematica/Assets/Scripts/Util/Xml.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+/// <summary>
+/// Saves an serializable object to a xml file or load a xml file into an object.
+/// </summary>
+public static class Xml {
+
+    /// <summary>
+    /// Saves an object to a xml file.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="path">The path.</param>
+    /// <param name="obj">The object.</param>
+    public static void Save<T> (string path, T obj) {
+        try {
+            XmlSerializer xs = new XmlSerializer(typeof(T));
+            XmlWriterSettings xws = new XmlWriterSettings();
+            Encoding encoding = Encoding.GetEncoding("UTF-8");
+            xws.CloseOutput = true;
+            using (StreamWriter stream = new StreamWriter(path, false, encoding)) {
+                xs.Serialize(stream, obj);
+                stream.Close();
+            }
+        }
+        catch (IOException e) {
+            Debug.LogException(e);
+        }
+    }
+
+    /// <summary>
+    /// Loads the specified file to a object.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="path">The path.</param>
+    /// <returns></returns>
+    public static T Load<T> (string path) {
+        try {
+            XmlSerializer xs = new XmlSerializer(typeof(T));
+            Encoding encoding = Encoding.GetEncoding("UTF-8");
+            using (StreamReader stream = new StreamReader(path, encoding)) {
+                T val = (T)xs.Deserialize(stream);
+                stream.Close();
+                return val;
+            }
+        }
+        catch (IOException e) {
+            Debug.LogException(e);
+            return default(T);
+        }
+        catch (InvalidCastException e) {
+            Debug.LogException(e);
+            return default(T);
+        }
+    }
+}
