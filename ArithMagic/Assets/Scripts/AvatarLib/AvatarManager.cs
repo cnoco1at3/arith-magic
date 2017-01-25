715a520dc982983684b12c02b42d10579b28d3bb
yihengz <chuyathang@outlook.com>
2017-01-25 15:26:45 -0500

    avatar manager

---
+++ ArithMagic/Assets/Scripts/AvatarLib/AvatarManager.cs
@@ -0,0 +1,12 @@
+﻿using AvatarLib;
+
+public class AvatarManager {
+
+    private static AvatarProfile curr_profile;
+    private static AvatarBackEnd backend;
+
+    public AvatarManager() {
+        backend = new AvatarBackEnd(false);
+        curr_profile = null;
+    }
+}
