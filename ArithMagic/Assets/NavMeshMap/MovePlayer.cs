3c9531f34b76716a2662550e87c74dfc26679b59
Griva Patel <gpatel@andrew.ad.cmu.edu>
2017-02-22 16:09:59 -0500

    nav mesh

---
+++ ArithMagic/Assets/NavMeshMap/MovePlayer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayer : MonoBehaviour {
+
+    //public Transform destination;
+
+    private NavMeshAgent agent;
+
+    // Use this for initialization
+    void Start () {
+
+        agent = gameObject.GetComponent<NavMeshAgent>();
+
+       //agent.SetDestination(destination.position);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray screenRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit; 
+            if (Physics.Raycast(screenRay, out hit))
+            {
+                Debug.Log(hit.transform.tag);
+                agent.SetDestination(hit.point);
+            }
+            //Debug.Log(GetComponent<Rigidbody>().velocity);
+        }
+
+    }
+}
