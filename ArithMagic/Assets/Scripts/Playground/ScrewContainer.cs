3f0e24908859dda1d1752e8cce75b17d02cd2111
yihengz <chuyathang@outlook.com>
2017-03-10 15:13:18 -0500

    game mechanic

--- ArithMagic/Assets/Scripts/Playground/ScrewContainer.cs
+++ ArithMagic/Assets/Scripts/Playground/ScrewContainer.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using InteractLib;
 
-public class ScrewContainer : MonoBehaviour {
+public class ScrewContainer : Clickable {
 
     [SerializeField]
     private int width_ = 5;
@@ -12,11 +13,17 @@ public class ScrewContainer : MonoBehaviour {
     [SerializeField]
     private const float scale_factor_ = 0.5f;
 
+    private ScrewBehaviour[] buckets_;
+
     private int slot_index_ = -1;
 
     // NOTE: We got two options here
     // 1. Align all the slots manually
     // 2. Procedurally align them
+
+    void Start() {
+        buckets_ = new ScrewBehaviour[width_ * height_];
+    }
 
     public bool IsFull() {
         return slot_index_ == width_ * height_ - 1;
@@ -26,9 +33,11 @@ public class ScrewContainer : MonoBehaviour {
         return slot_index_ == -1;
     }
 
-    public int ObtainSlot() {
-        slot_index_++;
-        return slot_index_ - 1;
+    public int ObtainSlot(ScrewBehaviour screw) {
+        if (IsFull())
+            return -1;
+        buckets_[++slot_index_] = screw;
+        return slot_index_;
     }
 
     public Vector3 GetNextSlotPosition() {
@@ -36,4 +45,16 @@ public class ScrewContainer : MonoBehaviour {
         return new Vector3((next_index % width_) * scale_factor_, (next_index / width_) * scale_factor_);
     }
 
+    public override void ClickEvent() {
+        ScrewBehaviour tmp = ReleaseSlot();
+        tmp.ReturnFromContainer();
+    }
+
+    private ScrewBehaviour ReleaseSlot() {
+        if (IsEmpty())
+            return null;
+        ScrewBehaviour tmp = buckets_[slot_index_];
+        buckets_[slot_index_--] = null;
+        return tmp;
+    }
 }
