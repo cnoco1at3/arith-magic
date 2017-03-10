cb4a6fb62177cbbefea26ed8a7f9610eca65fbce
albertal <aalbertgames@gmail.com>
2017-03-09 20:37:14 -0500

    Xray test
    Render texture, sprites, and materials for xray mechanic. Added two tags

---
+++ ArithMagic/Assets/Scripts/AaronTest/MousePosition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePosition : MonoBehaviour
+{
+    private Vector2 xrayPos;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        xrayPos = Input.mousePosition;
+        xrayPos = Camera.main.ScreenToWorldPoint(xrayPos);
+        transform.position = new Vector3(xrayPos.x, xrayPos.y, -1); 
+	}
+}
