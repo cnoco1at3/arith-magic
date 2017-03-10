a36235b10121034d58a73cbe45923f6c5818e402
albertal <aalbertgames@gmail.com>
2017-03-10 14:47:05 -0500

    XRay mechanic prototype
    Added xray mechanic using render textures. Added xray script that
    detects objects tagged part.

---
+++ ArithMagic/Assets/Scripts/AaronTest/XRay.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XRay : MonoBehaviour
+{
+    public GameObject brokenPart;
+    [SerializeField]
+    private int detectTime;
+    private int detectTimeStart;
+    private Coroutine detectCo; 
+
+    //OnTriggerEnter with broken part, starts a coroutine countdown
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Part")
+        {
+            Debug.Log("Enter");
+            detectTime = detectTimeStart;
+            brokenPart = other.gameObject;
+            detectCo = StartCoroutine(DetectPart()); 
+        }
+    }
+
+    //Exit with part, stops and resets countdown
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Part")
+        {
+            StopCoroutine(detectCo);
+            Debug.Log("Exit");
+            detectTime = detectTimeStart;
+        }
+    }
+
+    //Coroutine countdown for detecting broken part
+    private IEnumerator DetectPart()
+    {
+        while (true)
+        {
+            if (detectTime > 0)
+            {
+                Debug.Log("DetectTime - 1");
+                detectTime -= 1;
+                yield return new WaitForSeconds(1);
+            }
+
+            //at 0 starts funciton to spawn math problem
+            else if (detectTime == 0)
+            {
+                Debug.Log("DetectTime = 0");
+                StartMath();
+                yield return null;
+            }
+            
+        }
+
+    }
+
+    private void StartMath()
+    {
+        //do math stuff
+        Debug.Log("StartMath");
+        StopCoroutine(detectCo);
+        //Destroy(brokenPart);
+    }
+    // Use this for initialization
+    void Start ()
+    {
+        detectTimeStart = detectTime;
+	}
+	
+}
