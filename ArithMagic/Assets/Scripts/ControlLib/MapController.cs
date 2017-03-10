cdff75788a52400f1e8871fd58a55d1a56bd0e03
yihengz <chuyathang@outlook.com>
2017-03-10 17:25:33 -0500

    add some comment and input sprites

--- ArithMagic/Assets/Scripts/ControlLib/MapController.cs
+++ ArithMagic/Assets/Scripts/ControlLib/MapController.cs
@@ -6,11 +6,15 @@ using DG.Tweening;
 
 public class MapController : GenericSingleton<MapController> {
 
+    // points where robots are
     [SerializeField]
     private Transform[] route_points_;
+
+    // the avatar of the player
     [SerializeField]
     private GameObject sprite_;
 
+    // this is index of the level recorded in the map
     private static int map_level_index_ = 0;
 
     void Start() {
@@ -22,6 +26,9 @@ public class MapController : GenericSingleton<MapController> {
             TransFromMapLevelToControllerLevel(level_index);
     }
 
+    // if the level index in level controller is different from the level recorded in the map
+    // then we should start some animation in the map and unlock the later level until their
+    // indexes are same.
     private void TransFromMapLevelToControllerLevel(int level_index) {
         Debug.Assert(map_level_index_ != level_index);
         int step = map_level_index_ < level_index ? 1 : -1; // NP-Hard
