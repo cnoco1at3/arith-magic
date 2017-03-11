a2358ce4db03d8228069597b8ab503686373e948
albertal <aalbertgames@gmail.com>
2017-03-10 20:54:40 -0500

    Added start screen
    Start screen button will go to scene when pressed. Can change the name
    of target scene on the button object.

---
+++ ArithMagic/Assets/Scripts/AaronTest/PlayButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayButton : MonoBehaviour
+{
+    [SerializeField]
+    private string nextSceneName;
+    private Button playButton; 
+
+    public void StartButton()
+    {
+        StartCoroutine(StartGame());
+    }
+
+    private IEnumerator StartGame()
+    {
+        playButton.interactable = false; 
+        //sound effect
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(nextSceneName);
+    }
+
+    void Start()
+    {
+        playButton = GetComponent<Button>();
+    }
+}
