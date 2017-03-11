ecaecc283bfbf0aadf7fd1728c5edee394c0201f
yihengz <chuyathang@outlook.com>
2017-03-10 19:30:05 -0500

    dinner checkpoint

---
+++ ArithMagic/Assets/Scripts/Playground/BeltAnimator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class BeltAnimator : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] gears;
+
+    public void RotateGears() {
+        foreach(GameObject gear in gears) {
+            Vector3 current = gear.transform.localEulerAngles;
+            current -= new Vector3(0, 0, 180);
+            gear.transform.DOLocalRotate(current, 2);
+        }
+    }
+}
---
+++ ArithMagic/Assets/Scripts/Playground/MapScroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InteractLib;
+
+public class MapScroller : Dragable {
+
+    private Vector3 prev_world_pos;
+
+    public override void OnTouchEnter(Vector3 touch_pos) {
+        prev_world_pos = Camera.main.ScreenToWorldPoint(new Vector3(touch_pos.x, touch_pos.y, transform.position.z - Camera.main.transform.position.z));
+    }
+
+    public override void OnTouchStay(Vector3 touch_pos) {
+        Vector3 world_pos = Camera.main.ScreenToWorldPoint(new Vector3(touch_pos.x, touch_pos.y, transform.position.z - Camera.main.transform.position.z));
+        Vector3 delta_pos = world_pos - prev_world_pos;
+        prev_world_pos = world_pos;
+
+        // TODO: Add soft bound here
+        transform.position = new Vector3(transform.position.x + delta_pos.x, transform.position.y, transform.position.z);
+    }
+}
