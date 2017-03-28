67b6f072576b479a29a6196b69502c077ac85615
ANDREW\gpatel <griva458@gmail.com>
2017-03-28 18:23:58 -0400

    feedback

---
+++ ArithMagic/Assets/Feedback/tapAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tapAnimation : MonoBehaviour {
+
+    private bool keyPressed = false;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space")){
+            Debug.Log("space");
+            keyPressed = true;
+            transform.localScale -= new Vector3(0.05f, 0.05f, 0.05f);
+        } else if (keyPressed)
+        {
+            keyPressed = false;
+            transform.localScale += new Vector3(0.05f, 0.05f, 0.05f);
+        }
+        
+
+    }
+}
