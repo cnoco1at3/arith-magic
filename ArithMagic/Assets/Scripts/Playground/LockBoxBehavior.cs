37a96fc7362e8dfbdd94bb5bae9587e379ceaa35
yihengz <chuyathang@outlook.com>
2017-03-30 18:45:25 -0400

    add cover

--- ArithMagic/Assets/Scripts/Playground/LockBoxBehavior.cs
+++ ArithMagic/Assets/Scripts/Playground/LockBoxBehavior.cs
@@ -20,7 +20,6 @@ public class LockBoxBehavior : Clickable {
     public AudioClip touchBoxSound;
 
     public override void ClickEvent() {
-        Debug.Log("clicked!!");
         SoundManager.Instance.PlaySFX(touchBoxSound);
         if (unlocked_)
             MoveRobot();
