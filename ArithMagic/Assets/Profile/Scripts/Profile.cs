c42a94949623a906a1504b6cd38e13d49c717b2b
ANDREW\gpatel <griva458@gmail.com>
2017-04-03 15:57:40 -0400

    delete profile

---
+++ ArithMagic/Assets/Profile/Scripts/Profile.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Profile : MonoBehaviour {
+
+	//public InputField mainInputField;
+
+	//public List<Text> names = new List<Text>();
+	public GameObject ExistingPlayerPrefab1;
+	public GameObject ExistingPlayerPrefab2;
+	public GameObject ExistingPlayerPrefab3;
+	public Text Player1Text;
+	public Text Player2Text;
+	public Text Player3Text;
+	public GameObject AddPlayerPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+		if (ProfileInfo.playerName.Count == 0) {
+			
+			Instantiate (AddPlayerPrefab, new Vector3 (0, 5, 0), Quaternion.identity);
+		} else if (ProfileInfo.playerName.Count == 1) {
+			Player1Text.text = ProfileInfo.playerName[0];
+			Instantiate (ExistingPlayerPrefab1, new Vector3 (0, 5, 0), Quaternion.identity);
+			Instantiate (AddPlayerPrefab, new Vector3 (0, 1, 0), Quaternion.identity);
+			//AddPlayerPrefab.transform.position = new Vector3 (0, 0, 0);
+			//names [0].text = ProfileInfo.playerName [0];
+		} else if (ProfileInfo.playerName.Count == 2) {
+			Player1Text.text = ProfileInfo.playerName[0];
+			Player1Text.text = ProfileInfo.playerName[1];
+			Instantiate (ExistingPlayerPrefab1, new Vector3 (0, 5, 0), Quaternion.identity);
+			Instantiate (ExistingPlayerPrefab2, new Vector3 (0, 1, 0), Quaternion.identity);
+			Instantiate (AddPlayerPrefab, new Vector3 (0, -3, 0), Quaternion.identity);
+		} else {
+			Player1Text.text = ProfileInfo.playerName[0];
+			Player1Text.text = ProfileInfo.playerName[1];
+			Player1Text.text = ProfileInfo.playerName[2];
+			Instantiate (ExistingPlayerPrefab1, new Vector3 (0, 5, 0), Quaternion.identity);
+			Instantiate (ExistingPlayerPrefab2, new Vector3 (0, 1, 0), Quaternion.identity);
+			Instantiate (ExistingPlayerPrefab3, new Vector3 (0, -3, 0), Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+	}
+
+	void OnMouseDown()
+	{
+		Debug.Log ("testing");
+
+	}
+}
---
+++ ArithMagic/Assets/Profile/Scripts/ProfilePassword.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class ProfilePassword : MonoBehaviour {
+
+	private string answer = "";
+	public List<Button> buttons = new List<Button>();
+	private bool Up;
+	private int counter;
+	private bool wrongAnswerDetected = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (wrongAnswerDetected && counter < 5) {
+			if (Up) {
+				transform.Translate (0.5f, 0, 0);
+				Up = false;
+			} else {
+				transform.Translate (-0.5f, 0, 0);
+				Up = true;
+				counter++;
+			}
+		} else {
+			transform.position = new Vector3 (0,0,0);
+			wrongAnswerDetected = false;
+			counter = 0;
+		}
+		
+		if (answer == "53") {
+			SceneManager.LoadScene ("Profile");
+		} else {
+			if (answer.Length > 1 && answer != "53") {
+				answer = "";
+				wrongAnswerDetected = true;
+			}
+		}
+
+		
+	}
+
+	public void ButtonClicked() {
+		string buttonName = EventSystem.current.currentSelectedGameObject.name;
+		Debug.Log (buttonName);
+		answer += buttonName;
+	}
+
+//	public void OnMouseDown(){
+//		Debug.Log ("cancel");
+//		if (gameObject.tag == "cancel") {
+//			Debug.Log ("cancel");
+//		}
+//	}
+
+
+
+}
